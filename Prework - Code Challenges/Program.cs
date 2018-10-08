38c823d99b4a357a5d48c770b0b16f5c883cb40d
carlos cadena <carloscadena@live.com>
2018-10-07 19:16:36 -0700

    First code challenge working

--- Prework - Code Challenges/Program.cs
+++ Prework - Code Challenges/Program.cs
@@ -11,14 +11,13 @@ namespace Prework___Code_Challenges
         }
         public static void First()
         {
-            Console.WriteLine("Hello again");
             int[] nums = new int[5];
             int picked = 0;
-            for(int i = 0; i < nums.Length; i++)
+            for (int i = 0; i < nums.Length; i++)
             {
                 Console.Write("Pick a number between 1 and 10:");
                 string numInput = Console.ReadLine();
-                if(Int32.TryParse(numInput, out picked) && int.Parse(numInput) > 0 && int.Parse(numInput) <= 10)
+                if (Int32.TryParse(numInput, out picked) && int.Parse(numInput) > 0 && int.Parse(numInput) <= 10)
                 {
                     nums[i] = int.Parse(numInput);
                 }
@@ -28,11 +27,19 @@ namespace Prework___Code_Challenges
                     i = i - 1;
                 }
             }
+            Console.WriteLine($"Your list is [{nums[0]}, {nums[1]}, {nums[2]}, {nums[3]}, {nums[4]}]");
+            Console.WriteLine("Please pick one of the numbers from your list: ");
+            int newInput = int.Parse(Console.ReadLine());
+            int counter = 0;
             for (int i = 0; i < nums.Length; i++)
             {
-                Console.Write(nums[i]);
+                if (nums[i] == newInput)
+                {
+                    counter += 1;
+                }
             }
+            Console.WriteLine($"The score for this number is {newInput * counter}");
         }
-        
+
     }
 }
