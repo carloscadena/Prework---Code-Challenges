2dd472bf6638d5841e759dfd012a45794bd7c130
carlos cadena <carloscadena@live.com>
2018-10-07 18:49:42 -0700

    first commit

--- Prework - Code Challenges/Program.cs
+++ Prework - Code Challenges/Program.cs
@@ -7,6 +7,32 @@ namespace Prework___Code_Challenges
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            First();
         }
+        public static void First()
+        {
+            Console.WriteLine("Hello again");
+            int[] nums = new int[5];
+            int picked = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("Pick a number between 1 and 10:");
+                string numInput = Console.ReadLine();
+                if(Int32.TryParse(numInput, out picked) && int.Parse(numInput) > 0 && int.Parse(numInput) <= 10)
+                {
+                    nums[i] = int.Parse(numInput);
+                }
+                else
+                {
+                    Console.Write("That won't work. Try again.\n");
+                    i = i - 1;
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(nums[i]);
+            }
+        }
+        
     }
 }
